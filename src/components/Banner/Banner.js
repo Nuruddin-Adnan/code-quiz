43439eff7adb003207b1c7ea0d441e20e17f07c9
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-10-11 03:29:12 +0600

    banner design

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './banner.css';
+
+const Banner = () => {
+    return (
+        <div className='container-fluid banner text-center text-white'>
+            <h1>Take Challange Now!!</h1>
+            <p className='lead col-lg-6 mx-auto'>
+                Code Quiz is online quiz game website. You can start and take a Challange. Prove your skills. Its improve your knowledge and coding skils. Dont wast time any more. Eat, sleep, code.s Love us, keep with us.
+            </p>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import logo from '../../logo.png';
+import './header.css'
 
 const Header = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light shadow-sm">
-            <div className="container">
-                <Link to='/' className="navbar-brand" >Navbar </Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav ms-auto">
-                        <NavLink to='/topics' className="nav-link" aria-current="page" >Topics</NavLink>
-                        <NavLink to='/statistics' className="nav-link" >Statistics</NavLink>
-                        <NavLink to='/blog' className="nav-link" >Blog</NavLink>
+        <header className='header'>
+            <nav className="navbar navbar-expand-lg bg-light shadow-sm">
+                <div className="container">
+                    <Link to='/' className="navbar-brand" >
+                        <img src={logo} alt="" />
+                        Code Quiz
+                    </Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav ms-auto">
+                            <NavLink to='/topics' className="nav-link px-md-4" aria-current="page" >Topics</NavLink>
+                            <NavLink to='/statistics' className="nav-link px-md-4" >Statistics</NavLink>
+                            <NavLink to='/blog' className="nav-link px-md-4" >Blog</NavLink>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
     );
 };
 
