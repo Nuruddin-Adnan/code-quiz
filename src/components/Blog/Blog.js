57c066d9409811ea26e7409a3fcc4cfe69108e15
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-10-11 16:23:18 +0600

    blog page complet

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,8 +2,39 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <div>
-            <h2>Blog page</h2>
+        <div className="container my-5">
+            <div className="row gy-4">
+                <div className="col-xl-8 mx-auto">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3 className="text-info">Question: #1 What is the purpose of react router?</h3>
+                        </div>
+                        <div className="card-body">
+                            <p>React Router is a standard library for routing in React. It <strong>enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL.</strong></p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xl-8 mx-auto">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3 className="text-info">Question: #2 How does context api works?</h3>
+                        </div>
+                        <div className="card-body">
+                            <p>The React Context API is <strong> a way for a React app to effectively produce global variables that can be passed around</strong>. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-xl-8 mx-auto">
+                    <div className="card">
+                        <div className="card-header">
+                            <h3 className="text-info">Question: #3 What is useRef Hook in react?</h3>
+                        </div>
+                        <div className="card-body">
+                            <p>The useRef is <strong>a hook that allows to directly create a reference to the DOM element in the functional component</strong>. Syntax: const refContainer = useRef(initialValue); The useRef returns a mutable ref object. This object has a property called .</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,93 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+// import { Cell, Tooltip, Pie, PieChart, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const Statistics = () => {
+    const quizsTopics = useLoaderData();
+    const chartData = quizsTopics.data;
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+
+    const data = [
+        {
+            name: 'Page A',
+            uv: 4000,
+            pv: 2400,
+            amt: 2400,
+        },
+        {
+            name: 'Page B',
+            uv: 3000,
+            pv: 1398,
+            amt: 2210,
+        },
+        {
+            name: 'Page C',
+            uv: 2000,
+            pv: 9800,
+            amt: 2290,
+        },
+        {
+            name: 'Page D',
+            uv: 2780,
+            pv: 3908,
+            amt: 2000,
+        },
+        {
+            name: 'Page E',
+            uv: 1890,
+            pv: 4800,
+            amt: 2181,
+        },
+        {
+            name: 'Page F',
+            uv: 2390,
+            pv: 3800,
+            amt: 2500,
+        },
+        {
+            name: 'Page G',
+            uv: 3490,
+            pv: 4300,
+            amt: 2100,
+        },
+    ];
+
+    console.log(chartData);
+
+
     return (
-        <div>
-            <h2>Statistics page</h2>
+        <div className='container col-xl-6 my-5' style={{ height: '400px' }}>
+            {/* <PieChart width={700} height={250}>
+                <Pie data={chartData} dataKey="total" nameKey="name" cx="50%" cy="50%" outerRadius={80} fill="#82ca9d" label>
+                    {chartData.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+                <Tooltip />
+            </PieChart> */}
+
+            <ResponsiveContainer>
+                <BarChart
+
+                    data={chartData}
+                    margin={{
+                        top: 20,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    {/* <Bar dataKey="pv" stackId="a" fill="#8884d8" /> */}
+                    <Bar dataKey="total" stackId="a" fill="#4ca8cd" />
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
