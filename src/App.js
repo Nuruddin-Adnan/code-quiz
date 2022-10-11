23b7d5d36d5ad0bba3aca9607ef7f85d950b31a3
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-10-11 08:15:27 +0600

    fontawsome added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Blog from "./components/Blog/Blog";
 import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
 import Statistics from "./components/Statistics/Statistics";
 import Main from "./layouts/Main";
 
@@ -29,6 +30,10 @@ function App() {
           element: <Blog></Blog>
         }
       ]
+    },
+    {
+      path: '*',
+      element: <NotFound></NotFound>
     }
   ])
   return (
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFaceSadCry } from '@fortawesome/free-solid-svg-icons'
+
+const NotFound = () => {
+    return (
+        <div className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
+            <div className=' text-center'>
+                <FontAwesomeIcon icon={faFaceSadCry} className='text-muted' style={{ fontSize: '5rem' }} />
+                <i class="fa-solid fa-face-head-bandage"></i>
+                <h1>404</h1>
+                <h5>Page not found</h5>
+                <p>The page you are looking for does not exist</p>
+                <Link to='/' className='btn btn-lg btn-primary'>Home page</Link>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
