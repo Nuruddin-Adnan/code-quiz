8f5b7311626a07a35edf1c0e8a0a8792d6cbcf58
Nuruddin-Adnan <contact.codepixel@gmail.com>
2022-10-12 04:44:32 +0600

    speling correction on banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,7 +4,7 @@ import './banner.css';
 const Banner = () => {
     return (
         <div className='container-fluid banner text-center text-white'>
-            <h1>Take Challange Now!!</h1>
+            <h1>Take Challenge Now!!</h1>
             <p className='lead col-lg-6 mx-auto'>
                 Code Quiz is online quiz game website. You can start and take a Challange. Prove your skills. Its improve your knowledge and coding skils. Dont wast time any more. Eat, sleep, code.s Love us, keep with us.
             </p>
